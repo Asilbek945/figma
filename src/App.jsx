fc1f76395a56b2cc294c028e7efff5d048e11493
metaitschool448 <metaitschool448@gmail.com>
2026-06-30 17:55:11 +0500

    Asilbek

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div className='sayt'>
-      <div className="tort">
+      <div className="ikki">
 
+      
 
       </div>
     </div>
