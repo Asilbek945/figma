5875bb595e21b76b9e71d9cdab61ef54087b5b72
metaitschool448 <metaitschool448@gmail.com>
2026-07-02 15:19:37 +0500

    edfghjk

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,39 @@
 import React from 'react'
 import img1 from './Salom.png'
+import Samandar from './Samandar'
 const App = () => {
   return (
     <div className='sayt'>
       <div className="ikki">
-      <div className="tepa_A">
-        <div className="chap">
-        <div className="img">
-        <img className='w-[50px] h-[50px]' src={img1} alt="" />
+        <div className="tepa_A">
+          <div className="chap">
+              <img className='w-[50px] h-[50px]' src={img1} alt="" />
+            <ul>
+              <li>
+                <a href="./Abdurahmon.jsx">HOME</a>
+                <a href="./App.jsx">SERVICES</a>
+                <a href="./Firdavs.jsx">Kitchen Showroom</a>
+                <a href="./Mumar.jsx">Gallery</a>
+                <a href="./Mrizo.jsx">Testimonials</a>
+                <a href="./Umar.jsx">Trades</a>
+                <select>
+                  <option value="">Contact us</option>
+                  <option value="">Contact us1</option>
+                  <option value="">Contact us2</option>
+                </select>
+                <a href="./Samanadar.jsx">Coverage areas</a>
+                <a href="">(805) 323-9515</a>
+                <a href="https://instagramm.com"><i class="fa-brands fa-instagram"></i></a>
+              </li>
+            </ul>
+          </div>
         </div>
-          <ul>
-            <li>
-              <a href="./Abdurahmon.jsx">HOME</a>
-              <a href="./App.jsx">SERVICES</a>
-              <a href="./Firdavs.jsx">Kitchen Showroom</a>
-              <a href="./Mumar.jsx">Gallery</a>
-              <a href="./Mrizo.jsx">Testimonials</a>
-              <a href="./Umar.jsx">Trades</a>
-              <select>
-                <option value="">Contact us</option>
-                <option value="">Contact us1</option>
-                <option value="">Contact us2</option>
-              </select>
-              <a href="./Samanadar.jsx">Coverage areas</a>
-              <a href="">(805) 323-9515</a>
-              <a href=""><i class="fa-brands fa-instagram"></i></a>
-            </li>
-          </ul>
-<<<<<<< HEAD
+      <div className="ss">
+        
       </div>
-        </div>
-
-      <div className="header">
-        <div className="header1">
-        <h1>Projects</h1>
-        </div>
-        <div className="header2">
-        <h3>Quality and affordability are an integral part of Focil’s design and <br /> construction. Whether we are building a custom residence.</h3>
-        </div>
-      </div>
-   </div>
-   {/* /xecrtvugybihu */}
-
-
-
-
-
-
-
-
-
-
-
-=======
-        </div>
       </div>
 
-      </div>
->>>>>>> 98a76a3ce51c42d629b0fe5cae25cc16f90a8040
-
+      <Samandar />
 
 
     </div>
