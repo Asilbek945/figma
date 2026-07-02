4563d699b5b4e50c9141e9ea261cbd2f06b61cc0
metaitschool448 <aligangbot@gmail.com>
2026-07-02 15:40:57 +0500

    xcfhj

--- src/Samandar.jsx
+++ src/Samandar.jsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import img1 from './Salom.png'
-const App = () => {
-    return (
-        < 
-        <div className="navhed">
+// import React from 'react'
+// import img1 from './Salom.png'
+// const App = () => {
+//     return (
+//         < 
+//         <div className="navhed">
     
-          <div className="navbar">
-            <div className="navbarr">
-            <img src={rasm1} alt="" />
-            </div>
-            <div className="navbarrr">
-    <ul>
-                <li>
-                  <a href="./Abdurahmon.jsx">HOME</a>
-                  <a href="./App.jsx">SERVICES</a>
-                  <a href="./Firdavs.jsx">Kitchen Showroom</a>
-                  <a href="./Mumar.jsx">Gallery</a>
-                  <a href="./Mrizo.jsx">Testimonials</a>
-                  <a href="./Umar.jsx">Trades</a>
-                  <  select className='a' >
-                    <option className='a1' value="">Contact us</option>
-                    <option className='a1' value="">Contact us1</option>
-                    <option className='a1' value="">Contact us2</option>
-                  </select>
-                  <a href="./Samanadar.jsx">Coverage areas</a>
-                  <a href="">(805) 323-9515</a>
-                  <a href=""><i class="fa-brands fa-instagram"></i></a>
-                </li>
-              </ul>
-          </div>
-            </div>
+//           <div className="navbar">
+//             <div className="navbarr">
+//             <img src={rasm1} alt="" />
+//             </div>
+//             <div className="navbarrr">
+//     <ul>
+//                 <li>
+//                   <a href="./Abdurahmon.jsx">HOME</a>
+//                   <a href="./App.jsx">SERVICES</a>
+//                   <a href="./Firdavs.jsx">Kitchen Showroom</a>
+//                   <a href="./Mumar.jsx">Gallery</a>
+//                   <a href="./Mrizo.jsx">Testimonials</a>
+//                   <a href="./Umar.jsx">Trades</a>
+//                   <  select className='a' >
+//                     <option className='a1' value="">Contact us</option>
+//                     <option className='a1' value="">Contact us1</option>
+//                     <option className='a1' value="">Contact us2</option>
+//                   </select>
+//                   <a href="./Samanadar.jsx">Coverage areas</a>
+//                   <a href="">(805) 323-9515</a>
+//                   <a href=""><i class="fa-brands fa-instagram"></i></a>
+//                 </li>
+//               </ul>
+//           </div>
+//             </div>
     
-          <div className="header">
-            <div className="header1">
-            <h1>Projects</h1>
-            </div>
-            <div className="header2">
-            <h3>Quality and affordability are an integral part of Focil’s design and <br /> construction. Whether we are building a custom residence.</h3>
-            </div>
-          </div>
-       </div>
+//           <div className="header">
+//             <div className="header1">
+//             <h1>Projects</h1>
+//             </div>
+//             <div className="header2">
+//             <h3>Quality and affordability are an integral part of Focil’s design and <br /> construction. Whether we are building a custom residence.</h3>
+//             </div>
+//           </div>
+//        </div>
 
 
 
@@ -47,27 +47,27 @@ const App = () => {
 
 
 
-        <div className='sayt'>
-            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                <legend className="fieldset-legend">Login</legend>
+//         <div className='sayt'>
+//             <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+//                 <legend className="fieldset-legend">Login</legend>
 
-                <label className="label">numer</label>
-                <input type="number" className="input" placeholder="numer" />
+//                 <label className="label">numer</label>
+//                 <input type="number" className="input" placeholder="numer" />
 
-                <label className="label">tugilgan yil oy kun</label>
-                <input type="date" className="input" placeholder="t  y, o" />
+//                 <label className="label">tugilgan yil oy kun</label>
+//                 <input type="date" className="input" placeholder="t  y, o" />
 
-                <label className="label">Password</label>
-                <input type="password" className="input" placeholder="Password" />
+//                 <label className="label">Password</label>
+//                 <input type="password" className="input" placeholder="Password" />
 
-                <button className="btn btn-neutral mt-4">Login</button>
-            </fieldset>
+//                 <button className="btn btn-neutral mt-4">Login</button>
+//             </fieldset>
 
-        </div>
+//         </div>
 
 
 
-    )
-}
+//     )
+// }
 
-export default App
+// export default App
