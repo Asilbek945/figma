500d80a2aff65627746655eb2702f3db3930e1d6
metaitschool448 <metaitschool448@gmail.com>
2026-07-02 15:30:59 +0500

    xsdfghjk

--- src/Samandar.jsx
+++ src/Samandar.jsx
@@ -2,6 +2,51 @@ import React from 'react'
 import img1 from './Salom.png'
 const App = () => {
     return (
+        < 
+        <div className="navhed">
+    
+          <div className="navbar">
+            <div className="navbarr">
+            <img src={rasm1} alt="" />
+            </div>
+            <div className="navbarrr">
+    <ul>
+                <li>
+                  <a href="./Abdurahmon.jsx">HOME</a>
+                  <a href="./App.jsx">SERVICES</a>
+                  <a href="./Firdavs.jsx">Kitchen Showroom</a>
+                  <a href="./Mumar.jsx">Gallery</a>
+                  <a href="./Mrizo.jsx">Testimonials</a>
+                  <a href="./Umar.jsx">Trades</a>
+                  <  select className='a' >
+                    <option className='a1' value="">Contact us</option>
+                    <option className='a1' value="">Contact us1</option>
+                    <option className='a1' value="">Contact us2</option>
+                  </select>
+                  <a href="./Samanadar.jsx">Coverage areas</a>
+                  <a href="">(805) 323-9515</a>
+                  <a href=""><i class="fa-brands fa-instagram"></i></a>
+                </li>
+              </ul>
+          </div>
+            </div>
+    
+          <div className="header">
+            <div className="header1">
+            <h1>Projects</h1>
+            </div>
+            <div className="header2">
+            <h3>Quality and affordability are an integral part of Focil’s design and <br /> construction. Whether we are building a custom residence.</h3>
+            </div>
+          </div>
+       </div>
+
+
+
+
+
+
+
         <div className='sayt'>
             <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
                 <legend className="fieldset-legend">Login</legend>
